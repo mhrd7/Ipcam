28f521a411bfbc491f6affa8e7a6c82f9c681106
Alias <mhrd.taskulu@gmail.com>
2022-07-08 17:25:43 +0430

    android ready to sen data verified by wireshark on remote

---
+++ app/src/main/java/com/mehrdad/ipcam/Server_setting.java
@@ -0,0 +1,14 @@
+package com.mehrdad.ipcam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Server_setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_setting);
+    }
+}
